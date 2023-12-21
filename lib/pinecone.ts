2de26302b29d803b75bc89bae3f08d515164af12
AgentRittik <109760373+AgentRittik@users.noreply.github.com>
2023-12-21 06:17:12 +0530

    competed the setup of openAi API for getting the vector embeddigns of the text

--- lib/pinecone.ts
+++ lib/pinecone.ts
@@ -1,8 +1,11 @@
-import {Pinecone} from '@pinecone-database/pinecone'
+import {Pinecone , Vector} from '@pinecone-database/pinecone'
 import { log } from 'console';
 import { downlaodFromS3 } from './s3-server';
 import {PDFLoader} from 'langchain/document_loaders/fs/pdf';
+import md5 from 'md5';
 import { Document  , RecursiveCharacterTextSplitter} from '@pinecone-database/doc-splitter';
+import { getEmbeddings } from './embeddings';
+
 let pinecone : Pinecone | null = null;
 
 export const getPineconeClient = async() => {
@@ -42,6 +45,28 @@ export async function loadS3IntoPinecone(fileKey : string){
     return pages ;
 }
 
+async function embedDocument(doc : Document){
+    //getting the embeddings for the document using openAi
+    try{
+        const embeddings = await getEmbeddings(doc.pageContent);
+        const hash = md5(doc.pageContent)
+        return {
+            id : hash,
+            values : embeddings,
+            metadata : {
+                text : doc.metadata.text,
+                pageNumber : doc.metadata.pageNumber
+            }
+
+        } as Vector;
+
+    }
+    catch(error){
+        console.log('error in embedding the document',error);
+        throw error;
+        
+    }
+}
 export const turncateStringByBytes = (str : string , bytes : number) => {
     const enc = new TextEncoder();
     return new TextDecoder('utf-8').decode(enc.encode(str).slice(0,bytes)); // we are converting the string into bytes and then slicing it
